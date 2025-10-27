c8ddea0de4a1a9e5da3648bd1075c25ac8b55762
imranjoget <muhammad.imran@joget.com>
2025-10-27 12:23:15 +0800

    Updated the plugin version

--- src/main/java/org/joget/plugin/marketplace/Activator.java
+++ src/main/java/org/joget/plugin/marketplace/Activator.java
@@ -8,7 +8,7 @@ import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
 
-    public static final String VERSION = "7.0.8";
+    public static final String VERSION = "7.0.9";
 
     protected Collection<ServiceRegistration> registrationList;
 
